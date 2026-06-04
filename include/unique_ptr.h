f3b6227a120ca2bae348b2b4e475cd72551c067a
Logan Rose <logprose@umich.edu>
2026-06-03 22:17:21 -0400

    feat: updated ctors

--- include/unique_ptr.h
+++ include/unique_ptr.h
@@ -1,62 +1,52 @@
 #include <utility>
 
-
-namespace selfmade {
+namespace testing {
     template <typename T>
     struct DefaultDeleter {
-        void operator()(T* p) const { delete p; }
+        void operator()(T* p) { delete p; }
     };
 
     template <typename T, typename Deleter = DefaultDeleter<T>>
     class UniquePtr {
-    public:
-        UniquePtr() = default;
-        explicit UniquePtr(T* p, Deleter deleter) : data_{p}, deleter_{deleter} {}
-        ~UniquePtr() { deleter_(data_); }
-        UniquePtr(const UniquePtr& other) = delete;
-        UniquePtr& operator=(const UniquePtr& other) = delete;
-        UniquePtr(UniquePtr&& other) noexcept {
-            data_ = std::exchange(other.data_, nullptr);
-        }
+        public:
+            UniquePtr() = default;
+            explicit UniquePtr(T* p) : data_{p} {}
+            UniquePtr(T* p, Deleter deleter) : data_{p}, deleter_{deleter} {}
+            ~UniquePtr() { deleter_(data_); }
+            UniquePtr(const UniquePtr& other) = delete;
+            UniquePtr& operator=(const UniquePtr& other) = delete;
+            UniquePtr(UniquePtr&& other) noexcept {
+                data_ = std::exchange(other.data_, nullptr);
+            }
 
-        UniquePtr& operator=(UniquePtr&& other) noexcept {
-            if (this == &other) {
+            UniquePtr& operator=(UniquePtr&& other) noexcept {
+                if (this == &other) {
+                    return *this;
+                }
+                deleter_(data_);
+                data_ = std::exchange(other.data_, nullptr);
                 return *this;
             }
-            deleter_(data_);
-            data_ = std::exchange(other.data_, nullptr);
-            return *this;
 
-        }
-        
-        T* release() {
-            T* to_return = std::exchange(data_, nullptr);
-            return to_return;
-        }
-
-        T* get() const {
-            return data_;
-        }
-
-        void reset(T* p = nullptr) {
-            if (p == data_) {
-                return;
+            T* get() const { return data_; }
+            T* release() { return std::exchange(data_, nullptr); }
+            T* operator->() const { return data_; }
+            T& operator*() const { return *data_; }
+            void reset(T* p = nullptr) {
+                if (p == data_) {
+                    return;
+                }
+                deleter_(data_);
+                data_ = p;
             }
-            deleter_(data_);
-            data_ = p;
-        }
 
-        T& operator*() const { return *data_; };
-        T* operator->() const { return data_; };
-
-
-    private:
-        T* data_{nullptr};
-        [[no_unique_address]] Deleter deleter_;
+        private:
+            T* data_{nullptr};
+            Deleter deleter_{};
     };
-
+    
     template <typename T, typename... Args>
-    UniquePtr<T> make_shared(Args&&... args) {
+    UniquePtr<T> make_unique(Args&&... args) {
         return UniquePtr<T>(new T(std::forward<Args>(args)...));
     }
 }
