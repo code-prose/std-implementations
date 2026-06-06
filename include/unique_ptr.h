ebb6da05a8d94d4d5d4e9ea0b2fa42c06f10f773
Logan Rose <logprose@umich.edu>
2026-06-05 20:29:37 -0400

    feat: std::move for deleter in unique_ptr too

--- include/unique_ptr.h
+++ include/unique_ptr.h
@@ -1,6 +1,6 @@
 #include <utility>
 
-namespace testing {
+namespace i_am_not_caring {
     template <typename T>
     struct DefaultDeleter {
         void operator()(T* p) { delete p; }
@@ -10,13 +10,15 @@ namespace testing {
     class UniquePtr {
         public:
             UniquePtr() = default;
-            explicit UniquePtr(T* p) : data_{p} {}
+            explicit UniquePtr(T* p) : data_{p}, deleter_{} {}
             UniquePtr(T* p, Deleter deleter) : data_{p}, deleter_{deleter} {}
             ~UniquePtr() { deleter_(data_); }
             UniquePtr(const UniquePtr& other) = delete;
             UniquePtr& operator=(const UniquePtr& other) = delete;
+
             UniquePtr(UniquePtr&& other) noexcept {
                 data_ = std::exchange(other.data_, nullptr);
+                deleter_ = std::move(other.deleter_);
             }
 
             UniquePtr& operator=(UniquePtr&& other) noexcept {
@@ -25,13 +27,10 @@ namespace testing {
                 }
                 deleter_(data_);
                 data_ = std::exchange(other.data_, nullptr);
+                deleter_ = std::move(other.deleter_);
                 return *this;
             }
 
-            T* get() const { return data_; }
-            T* release() { return std::exchange(data_, nullptr); }
-            T* operator->() const { return data_; }
-            T& operator*() const { return *data_; }
             void reset(T* p = nullptr) {
                 if (p == data_) {
                     return;
@@ -40,11 +39,16 @@ namespace testing {
                 data_ = p;
             }
 
+            T* operator->() const { return data_; }
+            T* get() const { return data_; }
+            T& operator*() const { return *data_; }
+            T* release() { return std::exchange(data_, nullptr); }
+
         private:
             T* data_{nullptr};
             Deleter deleter_{};
     };
-    
+
     template <typename T, typename... Args>
     UniquePtr<T> make_unique(Args&&... args) {
         return UniquePtr<T>(new T(std::forward<Args>(args)...));
