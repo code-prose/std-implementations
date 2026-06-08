b9dd6edb0ea6fabfc04672d50ae8907ca8f2774f
Logan Rose <logprose@umich.edu>
2026-06-07 22:36:39 -0400

    feat: new shared_ptr impl

--- include/shared_ptr.h
+++ include/shared_ptr.h
@@ -1,45 +1,42 @@
 #include <atomic>
 #include <utility>
 
-namespace sharing_is_caring {
+namespace woo_rewrite {
     template <typename T>
     struct DefaultDeleter {
         void operator()(T* p) { delete p; }
     };
 
     struct ControlBlock {
-        std::atomic<size_t> use_count_{1};
+        std::atomic<std::size_t> use_count_{1};
     };
 
     template <typename T, typename Deleter = DefaultDeleter<T>>
     class SharedPtr {
         public:
             SharedPtr() = default;
-            explicit SharedPtr(T* p) : data_{p}, deleter_{}, cb_{new ControlBlock{}} {}
-            SharedPtr(T* p, Deleter deleter) : data_{p}, deleter_{deleter}, cb_{new ControlBlock{}} {}
-            ~SharedPtr() { 
-                release();
-            }
+            explicit SharedPtr(T* p) : data_{p}, cb_{new ControlBlock{}}, deleter_{} {}
+            SharedPtr(T* p, Deleter deleter) : data_{p}, cb_{new ControlBlock{}}, deleter_{deleter} {}
+            ~SharedPtr() { free(); }
 
             SharedPtr(const SharedPtr& other) {
                 cb_ = other.cb_;
                 data_ = other.data_;
                 deleter_ = other.deleter_;
-
                 if (cb_) increment();
-
             }
 
             SharedPtr& operator=(const SharedPtr& other) {
                 if (this == &other) {
                     return *this;
                 }
-                release();
+                free();
 
                 cb_ = other.cb_;
                 data_ = other.data_;
                 deleter_ = other.deleter_;
                 if (cb_) increment();
+
                 return *this;
             }
 
@@ -47,42 +44,55 @@ namespace sharing_is_caring {
                 cb_ = std::exchange(other.cb_, nullptr);
                 data_ = std::exchange(other.data_, nullptr);
                 deleter_ = std::move(other.deleter_);
-            } // move ctor.. nothing exists before
+
+            }
 
             SharedPtr& operator=(SharedPtr&& other) noexcept {
                 if (this == &other) {
                     return *this;
                 }
-
-                release();
-                
+                free();
 
                 cb_ = std::exchange(other.cb_, nullptr);
                 data_ = std::exchange(other.data_, nullptr);
                 deleter_ = std::move(other.deleter_);
+
                 return *this;
             }
 
-            void increment() {
-                cb_->use_count_.fetch_add(1); 
+
+
+            void reset(T* p = nullptr) {
+                free();
+                data_ = p;
+                cb_ = p ? new ControlBlock{} : nullptr;
             }
 
-            T* operator->() const { return data_; }
             T* get() const { return data_; }
+            T* operator->() const { return data_; }
             T& operator*() const { return *data_; }
-            // I might want to use a memory ordering to read this
-            std::size_t use_count() const { return std::atomic_load_explicit(&cb_->use_count_, std::memory_order_seq_cst); };
-
-            void release() {
-                if (cb_ && cb_->use_count_.fetch_sub(1) == 1) {
-                    deleter_(data_);
-                    delete cb_;
-                }
-            }
 
         private:
             T* data_{nullptr};
             ControlBlock* cb_{};
             Deleter deleter_{};
+
+            void free() {
+                if(cb_ && decrement() == 1) {
+                    deleter_(data_);
+                    delete cb_;
+                }
+                data_ = nullptr;
+                cb_ = nullptr;
+            }
+            void increment() { cb_->use_count_.fetch_add(1); }
+            std::size_t decrement() {
+                return cb_->use_count_.fetch_sub(1);
+            }
     };
+
+    template <typename T, typename... Args>
+    SharedPtr<T> make_shared(Args&&... args) {
+        return SharedPtr<T>(new T(std::forward<Args>(args)...));
+    }
 }
